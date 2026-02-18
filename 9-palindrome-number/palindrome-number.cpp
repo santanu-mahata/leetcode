667602cf8c7dcaf681612e362912ab6270fc7fe9
santanu <107527426+santanu-mahata@users.noreply.github.com>
2026-02-18 23:29:41 +0530

    Time: 2 ms (50.94%) | Memory: 8.5 MB (65.61%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+     if (x < 0 || (x % 10 == 0 && x != 0))
+            return false;
+
+        int revHalf = 0;
+
+        while (x > revHalf) {
+            revHalf = revHalf * 10 + x % 10;
+            x = x / 10;
+        }
+    return (x == revHalf || x == revHalf / 10);
+    }
+};
